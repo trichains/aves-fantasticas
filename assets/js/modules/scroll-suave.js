e3e02ef128ffb2e42e13a18b742e5d8545ee694d
Cristhian Almeida <scriptumbra@gmail.com>
2023-11-07 17:43:38 -0400

    att: algumas infos

---
+++ assets/js/modules/scroll-suave.js
@@ -0,0 +1,19 @@
+export default function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
