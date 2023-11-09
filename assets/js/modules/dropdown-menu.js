6fe46a24129430304a5519bd401050cb7befa8ed
Cristhian Almeida <3correntes@gmail.com>
2023-11-09 14:07:39 -0400

    add: estilos do dropdown-menu

--- assets/js/modules/dropdown-menu.js
+++ assets/js/modules/dropdown-menu.js
@@ -1,3 +1,17 @@
 export default function dropdownMenu() {
-  const dropdownMenu = document.querySelectorAll('[data-dropdown]');
+  const menus = document.querySelectorAll('[data-dropdown]');
+  
+  menus.forEach((menu) => {
+    ['click', 'touchstart'].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
+  });
+
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.toggle('ativo');
+    outsideClick(this, () => {
+      this.classList.remove('ativo');
+    });
+  }
 }
