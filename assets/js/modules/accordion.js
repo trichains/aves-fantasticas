33f76e3fd7aedeabef2198d32aa9867d6866ab75
Cristhian <trichains@hotmail.com>
2023-12-05 04:21:35 -0400

    Update accordion.js

--- assets/js/modules/accordion.js
+++ assets/js/modules/accordion.js
@@ -4,18 +4,18 @@ export default class Accordion {
     this.activeClass = 'ativo';
   }
 
- toggleAccordion(item) {
+  toggleAccordion(item) {
     item.classList.toggle(this.activeClass);
     item.nextElementSibling.classList.toggle(this.activeClass);
- }
-  
+  }
+
   // adiciona os eventos ao accordion
   addAccordionEvent() {
     this.accordionList.forEach((item) => {
       item.addEventListener('click', () => this.toggleAccordion(item));
     });
   }
-  
+
   // iniciar função
   init() {
     if (this.accordionList.length) {
@@ -23,5 +23,6 @@ export default class Accordion {
       this.toggleAccordion(this.accordionList[0]);
       this.addAccordionEvent();
     }
+    return this;
   }
 }
