a90dbe4e5289249f78a6f11636ce1a9df730efd9
Cristhian <trichains@hotmail.com>
2023-12-05 07:23:11 -0400

    fix dropdownmenu

--- assets/js/modules/dropdown-menu.js
+++ assets/js/modules/dropdown-menu.js
@@ -4,25 +4,27 @@ export default class DropdownMenu {
   constructor(dropdownMenus, events) {
     this.dropdownMenus = document.querySelectorAll(dropdownMenus);
 
-    // define touchstart e click como argumento padrao
+    // define touchstart e click como argumento padrão
+    // de events caso o usuário não define
     if (events === undefined) this.events = ['touchstart', 'click'];
     else this.events = events;
 
-    this.activeClass = 'active';
+    this.activeClass = 'ativo';
     this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
   }
 
-  // ativa o dropdown menu e adiciona a funcionalidade
+  // Ativa o dropdownmenu e adiciona
+  // a função que observa o clique fora dele
   activeDropdownMenu(event) {
     event.preventDefault();
     const element = event.currentTarget;
-    event.classList.toggle(this.activeClass);
+    element.classList.add(this.activeClass);
     outsideClick(element, this.events, () => {
-      element.classList.remove('active');
+      element.classList.remove('ativo');
     });
   }
 
-  // adiciona os eventos ao dropdown menu
+  // adiciona os eventos ao dropdownmenu
   addDropdownMenusEvent() {
     this.dropdownMenus.forEach((menu) => {
       this.events.forEach((userEvent) => {
